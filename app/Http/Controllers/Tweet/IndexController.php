2501dde9dbfa25ff9f2a035a4920a4dfa14b5767
kita127 <kitada.kosuke@gmail.com>
2022-07-27 13:48:12 +0900

    [2-1完了] DBからつぶやきを取得してHTMLで返す

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -16,6 +17,7 @@ class IndexController extends Controller
     public function __invoke(Request $request)
     {
         //
-        return view('tweet.index', ['name' => 'laravel']);
+        $tweets = Tweet::all();
+        return view('tweet.index')->with('tweets', $tweets);
     }
 }
--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -11,7 +11,11 @@
 
 <body>
     <h1>つぶやきアプリ</h1>
-    <p>{{ $name }}</p>
+    <div>
+        @foreach($tweets as $tweet)
+        <p>{{ $tweet->content }}</p>
+        @endforeach
+    </div>
 </body>
 
 </html>
